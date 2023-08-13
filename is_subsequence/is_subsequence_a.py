15c93b0b05fb0789b943d4066985f3a9a53e56b4
Nour Ayyoub <nour.ayyoub360@gmail.com>
2023-08-13 12:25:35 +0300

    is subsequence

---
+++ is_subsequence/is_subsequence_a.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        p_s, p_t = 0, 0
+        while p_s < len(s) and p_t < len(t):
+            if s[p_s] == t[p_t]:
+                p_s += 1
+            p_t += 1
+        if p_s == len(s):
+            return True
+        return False
